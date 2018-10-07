88c00332d292ba81de36bf0948f0b7818228d55a
CZA <2464851375@qq.com>
2018-10-07 19:43:06 +0800

    改进register_assembly

--- wxFEFactory/python/tools/assembly_hacktool.py
+++ wxFEFactory/python/tools/assembly_hacktool.py
@@ -70,88 +70,112 @@ class AssemblyHacktool(BaseHackTool):
         if not self.handler.active:
             return
 
-        original = item.original
-        raplace = item.raplace
-        assembly = item.assembly
-
         self.insure_memory()
 
         if item.key in self.registed_assembly:
             data = self.registed_assembly[item.key]
             addr = data['addr']
-            original = data['original']
-            memory = data['memory']
+            replace = data['replace']
             data['active'] = True
         else:
+            original = item.original
+            replace = item.replace
+            assembly = item.assembly
+            replace_len = item.replace_len
+            replace_offset = item.replace_offset
+
             addr = self.find_address(original, item.find_start, item.find_end, item.find_range_from_base, item.fuzzy)
             if addr is -1:
                 return
+
             memory = self.next_usable_memory
-            if item.replace_len:
-                original = original[:item.replace_len]
-            data = self.registed_assembly[item.key] = {'addr': addr, 'original': original,
-                'memory': memory, 'active': True}
-
-        if item.inserted:
             original_len = len(original)
-            available_len = original_len - len(raplace)  # 可用于跳转到插入的代码的jmp指令的长度
-            # 使用参数(暂时支持4字节)
-            if item.args:
-                memory_conflict = memory == self.next_usable_memory
-                if isinstance(assembly, AssemblyGroup):
-                    for arg in item.args:
-                        self.register_variable(arg)
+            read_original = False  # 是否需要通过handler读取最新的original
+            if replace_offset:
+                if replace_len is 0:
+                    if 0 < replace_offset < original_len:
+                        replace_len = original_len - replace_offset
+                    else:
+                        raise ValueError('此情况下replace_offset依赖于replace_len参数')
+                if replace_offset > 0 and replace_len <= original_len - replace_offset and not item.fuzzy:
+                    original = original[replace_offset: replace_len + replace_offset]
                 else:
-                    if self.next_usable_memory > 0xFFFFFFFF:
-                        raise ValueError('目前只支持32位参数地址')
-                    assembly = assembly % tuple(self.register_variable(arg).to_bytes(4, 'little') for arg in item.args)
-                if memory_conflict:
-                    memory = data['memory'] = self.next_usable_memory
-
-            # 动态生成机器码
-            if isinstance(assembly, AssemblyGroup):
-                assembly = assembly.generate(self, memory)
-
-            jmp_offset = memory - (addr + 5)
-            if abs(jmp_offset) < 0x7FFFFFFF or self.is32process:
-                # E9 relative address
-                # 计算jump地址, 5是jmp opcode的长度
-                jmp_len = 5
-                diff_new = utils.u32(jmp_offset)
-                diff_back = utils.u32(addr + original_len - (memory + len(assembly) + 5))
-                raplace += raplace + b'\xE9' + diff_new.to_bytes(4, 'little')
-                assembly = assembly + b'\xE9' + diff_back.to_bytes(4, 'little')
+                    read_original = True
+                addr += replace_offset
+                original_len = replace_len
             else:
-                if available_len >= 14:
-                    jmp_len = 14
-                    # FF25 00000000 absolute address
-                    raplace += raplace + b'\xFF\x25\x00\x00\x00\x00' + memory.to_bytes(8, 'little')
-                elif available_len >= 7 and self.next_usable_memory < 0xFFFFFFFF:
-                    # ptr jmp
-                    jmp_len = 7
+                if replace_len:
+                    original_len = replace_len
+                if item.fuzzy:
+                    read_original = True
+                elif replace_len:
+                    original = original[:original_len]
+
+            if read_original:
+                original = self.handler.read(addr, bytes, original_len)
+
+            if item.inserted:
+                available_len = original_len - len(replace)  # 可用于跳转到插入的代码的jmp指令的长度
+                # 使用参数(暂时支持4字节)
+                if item.args:
                     memory_conflict = memory == self.next_usable_memory
-                    temp = self.register_variable(item.key + '_jmp', 8)
+                    if isinstance(assembly, AssemblyGroup):
+                        for arg in item.args:
+                            self.register_variable(arg)
+                    else:
+                        if self.next_usable_memory > 0xFFFFFFFF:
+                            raise ValueError('目前只支持32位参数地址')
+                        assembly = assembly % tuple(self.register_variable(arg).to_bytes(4, 'little')
+                            for arg in item.args)
                     if memory_conflict:
-                        memory = data['memory'] = self.next_usable_memory
-                        self.handler.write_ptr(temp, memory)
-                    raplace += b'\xFF\x24\x25' + temp.to_bytes(4, 'little')
+                        memory = self.next_usable_memory
+
+                # 动态生成机器码
+                if isinstance(assembly, AssemblyGroup):
+                    assembly = assembly.generate(self, memory)
+
+                jmp_offset = memory - (addr + 5)
+                if abs(jmp_offset) < 0x7FFFFFFF or self.is32process:
+                    # E9 relative address
+                    # 计算jump地址, 5是jmp opcode的长度
+                    jmp_len = 5
+                    diff_new = utils.u32(jmp_offset)
+                    diff_back = utils.u32(addr + original_len - (memory + len(assembly) + 5))
+                    replace += replace + b'\xE9' + diff_new.to_bytes(4, 'little')
+                    assembly = assembly + b'\xE9' + diff_back.to_bytes(4, 'little')
                 else:
-                    raise ValueError('不支持当前情况jmp')
+                    if available_len >= 14:
+                        jmp_len = 14
+                        # FF25 00000000 absolute address
+                        replace += replace + b'\xFF\x25\x00\x00\x00\x00' + memory.to_bytes(8, 'little')
+                    elif available_len >= 7 and self.next_usable_memory < 0xFFFFFFFF:
+                        # ptr jmp
+                        jmp_len = 7
+                        memory_conflict = memory == self.next_usable_memory
+                        temp = self.register_variable(item.key + '_jmp', 8)
+                        if memory_conflict:
+                            memory = self.next_usable_memory
+                            self.handler.write_ptr(temp, memory)
+                        replace += b'\xFF\x24\x25' + temp.to_bytes(4, 'little')
+                    else:
+                        raise ValueError('不支持当前情况jmp')
 
-                assembly = assembly + b'\xFF\x25\x00\x00\x00\x00' + (addr + original_len).to_bytes(8, 'little')
+                    assembly = assembly + b'\xFF\x25\x00\x00\x00\x00' + (addr + original_len).to_bytes(8, 'little')
 
-            if available_len < jmp_len:
-                raise ValueError("可用长度不足以插入jmp代码")
+                if available_len < jmp_len:
+                    raise ValueError("可用长度不足以插入jmp代码")
+                elif available_len != jmp_len:
+                    # 填充的NOP
+                    replace += b'\x90' * (available_len - jmp_len)
 
-            # 填充的NOP
-            raplace += b'\x90' * (available_len - jmp_len)
+                if memory == self.next_usable_memory:
+                    self.next_usable_memory += utils.align4(len(assembly))
+                self.handler.write(memory, assembly)
 
-            if memory == self.next_usable_memory:
-                self.next_usable_memory += utils.align4(len(assembly))
+            data = self.registed_assembly[item.key] = {'active': True, 'addr': addr, 'original': original,
+                'replace': replace, 'memory': memory}
 
-            self.handler.write(memory, assembly)
-
-        self.handler.write(addr, raplace)
+        self.handler.write(addr, replace)
 
     def unregister_assembly(self, key):
         """恢复机器码修改"""
@@ -213,7 +237,7 @@ class AssemblyItems:
     :param original: 原始数据
     :param find_start: 原始数据查找起始
     :param find_end: 原始数据查找结束
-    :param raplace: 原始数据替换为的内容
+    :param replace: 原始数据替换为的内容
     :param assembly: 写到新内存的内容
     :param find_range_from_base: 是否将find_start和find_end加上模块起始地址
     :param inserted: 是否自动加入jmp代码
@@ -221,14 +245,15 @@ class AssemblyItems:
 """
 AssemblyItem = DataClass(
     'AssemblyItem',
-    ('key', 'label', 'original', 'find_start', 'find_end', 'raplace', 'assembly',
-        'find_range_from_base', 'inserted', 'fuzzy', 'replace_len', 'args'),
+    ('key', 'label', 'original', 'find_start', 'find_end', 'replace', 'assembly',
+        'find_range_from_base', 'inserted', 'fuzzy', 'replace_len', 'replace_offset', 'args'),
     defaults={
         'assembly': None,
         'find_range_from_base': True,
         'inserted': False,
         'fuzzy': False,
         'replace_len': 0,
+        'replace_offset': 0,
         'args': ()
     }
 )
