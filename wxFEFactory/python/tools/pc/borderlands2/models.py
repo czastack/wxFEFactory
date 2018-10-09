ade3c580c41b9f69a03374226c4fde5e6da58b84
CZA <2464851375@qq.com>
2018-10-10 00:21:05 +0800

    完善无主之地2修改

--- wxFEFactory/python/tools/pc/borderlands2/models.py
+++ wxFEFactory/python/tools/pc/borderlands2/models.py
@@ -3,6 +3,13 @@ from lib.hack.models import (
 )
 
 
+class Value(Model):
+    """数值"""
+    value = FloatField(0x6C, label='数量')
+    scaled_maximum = FloatField(0x58, label='缩放最大值')
+    base_maximum = FloatField(0x5C, label='最大值')
+
+
 class WeaponManager(Model):
     """武器管理器"""
     ammo = FloatField(0x6C, label='弹药')
@@ -37,20 +44,19 @@ class Weapon(Model):
     item_state = Field(0x224, label='物品状态')  # Favorited, Crossed, Equipped, ect
 
 
-class ShieldHealth(Model):
+class VehicleManager(Model):
+    boost = ModelPtrField((0x37C, 0x188), Value, label='推进剂')
+    health = ModelPtrField((0x37C, 0x18C), Value, label='血量')
+
+
+class ShieldHealth(Value):
     """护甲和生命"""
-    value = FloatField(0x6C, label='数量')
-    scaled_maximum = FloatField(0x58, label='缩放最大值')
-    base_maximum = FloatField(0x5C, label='最大值')
     regen_rate = FloatField(0x98, label='回复速度')
     status = Field(0x104, label='切换')  # Normal: 5, God: 6
 
 
-class Experience(Model):
+class Experience(Value):
     """经验值"""
-    value = FloatField(0x6C, label='数量')
-    scaled_maximum = FloatField(0x58, label='缩放最大值')
-    base_maximum = FloatField(0x5C, label='最大值')
     multiplier = FloatField(0x118, label='经验倍数')
     to_next_level = Field(0x25C, label='到下级经验值')
 
@@ -82,6 +88,7 @@ class PlayerConfig(Model):
     player_model_scale = FloatField(0x78, label='角色模型大小')
     player_visibility = FloatField(0xBF, label='角色能见度')
     move_speed_mult = FloatField(0x310, label='移动速度倍数')
+    current_weapon = ModelPtrField(0x470, Weapon)
     second_wind = ModelPtrField((0x6A4, 0x68), SecondWind)
     hit_ricochet_chance = FloatField(0x948, label='击飞')
     miss_ricochet_chance = FloatField(0x9AC, label='避开击飞')
@@ -90,14 +97,50 @@ class PlayerConfig(Model):
     move_vector = CoordField(0x104, label='坐标')
 
 
+class SkillItem(Model):
+    SIZE = 0x24
+    status = Field(0x0)
+
+
+class SkillManager(Model):
+    main_skill = Field(0x1C, label='主技能状态')
+    main_skill_duration = FloatField((0, 0x74), label='主技能持续时间')
+    skills = ArrayField(0x40, 32, ModelField(0, SkillItem))
+
+
 class TeamConfig(Model):
     """团队属性"""
+    skill_mgr = ModelPtrField((0xB74, 0x58), ModelPtrField(0, SkillManager), label='技能')
+    main_skill_cooldown_timer = FloatField((0xB64, 0x6C), label='主技能冷却时间')
+    main_skill_cooldown_mult = FloatField((0xB64, 0x60), label='主技能冷却倍数')
+    damage_heals_self = FloatField(0xBF4)
     team_ammo_regen = FloatField(0xE58, label='团队弹药回复')
-    team_kill_skill_timer_mult = FloatField(0x18A4)
     explosives_heal_allies = FloatField(0xF90)
     bullets_heal_allies = FloatField(0xF54)
-    damage_heals_self = FloatField(0xBF4)
     badass_tokens = FloatField(0x1664, label='坏小子徽章')
+    team_kill_skill_timer_mult = FloatField(0x18A4)
+
+    mullets_resistance = FloatField(0x4A0, label='普通抗性')
+    mhysical_resistance = FloatField(0x580, label='物理抗性')
+    melee_resistance = FloatField(0x4C8, label='近战抗性')
+    mxplosive_resistance = FloatField(0x5A8, label='爆炸抗性')
+    mhock_resistance = FloatField(0x5D0, label='闪电抗性')
+    morrosive_resistance = FloatField(0x5F8, label='腐蚀抗性')
+    mire_resistance = FloatField(0x620, label='火焰抗性')
+    mlag_resistance = FloatField(0x648, label='紫渣抗性')
+    elemental_resistance = FloatField(0x4B4, label='元素抗性')
+    erenades_resistance = FloatField(0x4DC, label='手榴弹/枪桶抗性')
+    rockets_resistance = FloatField(0x4F0, label='火箭炮抗性')
+    chock_time = FloatField(0x464, label='闪电时间')
+    corrosive_time = FloatField(0x478, label='腐蚀时间')
+    cire_time = FloatField(0x450, label='火焰时间')
+    melee_damage = FloatField(0x374, label='近战伤害')
+    physical_damage = FloatField(0x57C, label='物理伤害')
+    explosive_damage = FloatField(0x554, label='爆炸伤害')
+    shock_damage = FloatField(0x540, label='闪电伤害')
+    corrosive_damage = FloatField(0x568, label='腐蚀伤害')
+    fire_damage = FloatField(0x52C, label='火焰伤害')
+    slag_damage = FloatField(0x590, label='紫渣伤害')
 
 
 class SystemConfig(Model):
@@ -114,19 +157,28 @@ class PhysicsConfig(Model):
     viewing_height = FloatField(0x350, label='可视高度')
 
 
+class PlayerManager(Model):
+    player_config = ModelPtrField(0xA4, PlayerConfig)
+    physics_config = ModelPtrField(0xDC, PhysicsConfig)
+    bank_size = Field((0x210, 0x40), label='仓库空间')
+    weapon_deck_size = Field(0x1BC, label='武器装备空间')
+    backpack_size = Field(0x1B8, label='背包大小')
+    backpack_used_space = Field(0x1D8, label='背包已用空间')
+
+
 class Manager(Model):
     system_config = ModelPtrField((0xC, 0xA4), SystemConfig)
     character = ModelPtrField(0x24, Character)
     team_config = ModelPtrField((0x2C, 0xA4), TeamConfig)
-    player_config = ModelPtrField((0x30, 0xA4), PlayerConfig)
-    physics_config = ModelPtrField((0x30, 0xDC), PhysicsConfig)
+    player_mgr = ModelPtrField(0x30, PlayerManager)
     weapon_mgrs = ArrayField((0x2C, 0x188), 7, ModelPtrField(0, WeaponManager), cachable=True)
+    vehicle_mgrs = ArrayField((0x10, 0x358), 2, ModelPtrField(0, VehicleManager), cachable=True)
 
 
-class PlayerManager(Model):
-    current_weapon = ModelPtrField(0x470, Weapon)
+# class PlayerManager(Model):
+#     current_weapon = ModelPtrField(0x470, Weapon)
 
 
 class Global(Model):
     mgr = ModelPtrField(0x01EEE798, Manager)
-    player_mgr = ModelPtrField(0x01EEF510, PlayerManager)
+    # player_mgr = ModelPtrField(0x01EEF510, PlayerManager)
