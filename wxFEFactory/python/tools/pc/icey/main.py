7daed960844e188540b58d30ffbd4dfb8dd0238b
chenzhenan <chenzhenan@minieye.cc>
2020-10-09 15:42:11 +0800

    feat: 改进ICEY修改

--- wxFEFactory/python/tools/pc/icey/main.py
+++ wxFEFactory/python/tools/pc/icey/main.py
@@ -2,9 +2,9 @@ from lib.hack.forms import (
     Group, StaticGroup
 )
 from lib import ui
-from tools.base.assembly_hacktool import AssemblyItem
+from tools.base.assembly_hacktool import AssemblyItem, Delta
 from tools.base.mono_hacktool import MonoHacktool
-# from . import models, datasets
+from . import models
 
 
 class Main(MonoHacktool):
@@ -14,37 +14,28 @@ class Main(MonoHacktool):
     def onattach(self):
         super().onattach()
 
-        PlayerAttribute, UIDataBind = self.get_global_mono_classes((
-            "PlayerAttribute",
-            "UIDataBind",
+        self.register_classes((
+            models.PlayerAttribute,
+            models.UIDataBind,
         ))
-
-        (
-            self.set_currentEnergy_native,
-            self.set_currentHP_native,
-            self.UpdateCharInfo_native,
-        ) = self.get_mono_compile_methods(self.get_mono_methods((
-            (PlayerAttribute, "set_currentEnergy", 1),
-            (PlayerAttribute, "set_currentHP", 1),
-            (UIDataBind, "UpdateCharInfo", 0),
-        )))
+        self.assembly_address_dict = {
+            'unlimited_health': models.PlayerAttribute.set_currentEnergy.mono_compile,
+            'unlimited_energy': models.PlayerAttribute.set_currentHP.mono_compile,
+            'unlimited_money': models.UIDataBind.UpdateCharInfo.mono_compile + 0x200,
+        }
 
     def render_main(self):
-        Group("player", "全局", None)
         self.lazy_group(StaticGroup("代码插入"), self.render_assembly_buttons_own)
 
     def render_assembly_buttons_own(self):
+        delta = Delta(0x400)
+
         self.render_assembly_buttons((
-            AssemblyItem('unlimited_health', '无限生命', b'\x89\x47\x20\x48\x8B\x7D\xF8',
-                self.set_currentHP_native, self.set_currentHP_native + 0x100,
-                b'', b'\x8B\x47\x1C\x89\x47\x20\x48\x8B\x7D\xF8',
-                inserted=True, find_base=False),
-            AssemblyItem('unlimited_energy', '无限能量', b'\x89\x47\x28\x48\x8B\x7D\xF8',
-                self.set_currentEnergy_native, self.set_currentEnergy_native + 0x100,
-                b'', b'\x8B\x47\x24\x89\x47\x28\x48\x8B\x7D\xF8',
-                inserted=True, find_base=False),
-            AssemblyItem('unlimited_money', '无限金钱', b'\x48\x63\x49\x10\x3B\xC1',
-                self.UpdateCharInfo_native + 0x200, self.UpdateCharInfo_native + 0x400,
+            AssemblyItem('unlimited_health', '无限生命', b'\x89\x47\x20\x48\x8B\x7D\xF8', None, delta,
+                b'', b'\x8B\x47\x1C\x89\x47\x20\x48\x8B\x7D\xF8', inserted=True, find_base=False),
+            AssemblyItem('unlimited_energy', '无限能量', b'\x89\x47\x28\x48\x8B\x7D\xF8', None, delta,
+                b'', b'\x8B\x47\x24\x89\x47\x28\x48\x8B\x7D\xF8', inserted=True, find_base=False),
+            AssemblyItem('unlimited_money', '无限金钱', b'\x48\x63\x49\x10\x3B\xC1', None, delta,
                 b'', b'\xC7\x41\x10\x9F\x86\x01\x00\x48\x63\x49\x10\x39\xC8',
                 replace_len=6, inserted=True, find_base=False),
         ))
