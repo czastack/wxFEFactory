d8634c40f3fcb4284ac2b4b54cdb82cda558bb80
chenzhenan <chenzhenan@minieye.cc>
2020-10-20 19:51:14 +0800

    feat: 优化

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,12 @@
 #include "pch.h"
 
+
+/*int wmain(int argc, wchar_t* argv[])
+{
+	_putenv_s("PYTHONPATH", "python");
+	return Py_Main(argc, argv);
+}*/
+
 extern "C" int WINAPI wWinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -7,17 +14,33 @@ extern "C" int WINAPI wWinMain(
 	_In_ int nShowCmd
 )
 {
-	SetEnvironmentVariable(L"PYTHONPATH", L"python");
+	if (__argc > 1)
+	{
+		return Py_Main(__argc, __wargv);
+	}
+
+	_putenv_s("PYTHONPATH", "python");
 	Py_Initialize();
 
-	PyObject * pModule = PyImport_ImportModule("main2");
+	/*PyObject * pModule = PyImport_ImportModule("main");
 
 	if (pModule == NULL) {
 		if (PyErr_Occurred())
 			PyErr_Print();
-	}
+	}*/
 
-	if (Py_FinalizeEx() < 0) {
-		exit(120);
+	if (Py_IsInitialized())
+	{
+		FILE* fp;
+		fopen_s(&fp, "python/main2.py", "rb");
+
+		if (fp)
+		{
+			PyRun_AnyFileEx(fp, "main", 1);
+		}
+
+		if (Py_FinalizeEx() < 0) {
+			exit(120);
+		}
 	}
 }
